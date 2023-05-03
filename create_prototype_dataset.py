f5b5596ddf1b0dded3b855ad6d02731394b83382
stirlitzzz <ymulman@yahoo.com>
2023-05-03 16:31:26 -0400

    added the notebook with description and dataset generation

--- create_prototype_dataset.py
+++ create_prototype_dataset.py
@@ -49,11 +49,24 @@ def main(config):
         load_data.print_sf3_dims(ticker_list,"ticker_list")
         load_data.print_sf3_dims(date_list,"date_list")
         load_data.print_sf3_dims(investorname_name_list,"investorname_name_list")
-    sf3_small = create_small_debug_sf3(sf3,3,len(date_list), 3)
+    #sf3_small = create_small_debug_sf3(sf3,3,len(date_list), 3)
+    #parse arguments to get the number of tickers, dates, and investor names and file paths
+    #create the filenames for the small dataset as well as for the train, val, and test datasets
+    #the filenames should include the number of tickers, dates, and investor names
+
+    filepath=config.file_path
+    num_tickers = config.num_stocks
+    num_dates = config.num_quarters
+    if (config.use_all_quarters):
+        num_dates = len(date_list)
+    num_investor_names = config.num_investornames
+    filename_small = f'{filepath}_small_t{num_tickers}_i{num_investor_names}_q{num_dates}'
+    sf3_small = create_small_debug_sf3(sf3,num_tickers,num_dates, num_investor_names)
+
     if(config.debug):
         print(f'sf3_small.head()={sf3_small.head()}')
         print (f'sf3_small.shape={sf3_small.shape}')
-    load_data.save_df_to_csv(sf3_small,'data/SHARADAR_holdings_small.csv')
+    load_data.save_df_to_csv(sf3_small,f'{filename_small}_all.csv')
 
     sf3_train, sf3_val, sf3_test = load_data.split_train_val_test(sf3_small, 0.8, 0.1, 0.1, date_list)
     if(config.debug):
@@ -63,26 +76,19 @@ def main(config):
         print (f'sf3_val.shape={sf3_val.shape}')
         print(f'sf3_test.head()={sf3_test.head()}')
         print (f'sf3_test.shape={sf3_test.shape}')
-    load_data.save_df_to_csv(sf3_train,'data/SHARADAR_holdings_train.csv')
-    load_data.save_df_to_csv(sf3_val,'data/SHARADAR_holdings_val.csv')
-    load_data.save_df_to_csv(sf3_test,'data/SHARADAR_holdings_test.csv')
+    load_data.save_df_to_csv(sf3_train,f'{filename_small}_train.csv')
+    load_data.save_df_to_csv(sf3_val,f'{filename_small}_val.csv')
+    load_data.save_df_to_csv(sf3_test,f'{filename_small}_test.csv')
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--num_classes", type=int, default=5)
-    parser.add_argument("--num_shot", type=int, default=1)
-    parser.add_argument("--num_workers", type=int, default=4)
-    parser.add_argument("--eval_freq", type=int, default=100)
-    parser.add_argument("--meta_batch_size", type=int, default=128)
-    parser.add_argument("--hidden_dim", type=int, default=128)
-    parser.add_argument("--random_seed", type=int, default=123)
-    parser.add_argument("--learning_rate", type=float, default=1e-3)
-    parser.add_argument("--train_steps", type=int, default=25000)
-    parser.add_argument("--image_caching", type=bool, default=True)
-    parser.add_argument("--device", type=str, default="cpu")
+    parser.add_argument("--num_stocks", type=int, default=100)
+    parser.add_argument("--num_investornames", type=int, default=100)
+    parser.add_argument("--num_quarters", type=int, default=4)
+    parser.add_argument("--use_all_quarters", type=bool, default=True)
     parser.add_argument("--debug", type=str, default=False)
-    parser.add_argument('--cache', action='store_true')
+    parser.add_argument("--file_path", type=str, default='data/sf3_small')
 
     args = parser.parse_args()
 
